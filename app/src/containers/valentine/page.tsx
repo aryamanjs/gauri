bae9fe9ad1eec1a723aad85cfcb26dd85ffc44b7
Aryaman Sarkar <aryamansarkar30@gmail.com>
2025-07-29 23:18:53 +0530

    Update page.tsx

--- app/src/containers/valentine/page.tsx
+++ app/src/containers/valentine/page.tsx
@@ -56,7 +56,7 @@ export default function Valentine() {
             />
           </div>
           <div className="text-2xl mt-2 text-gray-900 text-center">
-            Will you be my Valentine?
+             If I came all that way just to see you... would you let me take you out?
           </div>
           <div className="flex items-center justify-center mt-3 text-center">
             <button
